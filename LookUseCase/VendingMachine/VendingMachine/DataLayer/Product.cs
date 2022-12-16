d41680d7cd7a45b391ab23974cad6489178f718c
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2022-12-16 22:30:16 +0200

    I fixed some issues

---
+++ LookUseCase/VendingMachine/VendingMachine/DataLayer/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace iQuest.VendingMachine
+{
+    internal class Product
+    {
+        public int ColumnId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantit { get; set; }
+    }
+}
