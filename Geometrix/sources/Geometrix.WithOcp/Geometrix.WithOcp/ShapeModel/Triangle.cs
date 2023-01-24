3ea060d0a1a958b9f3169dd6d2578198bca3ba9a
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2023-01-24 03:42:35 +0200

    Geometrix_Homework

---
+++ Geometrix/sources/Geometrix.WithOcp/Geometrix.WithOcp/ShapeModel/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iQuest.Geometrix.WithOcp.ShapeModel
+{
+    internal class Triangle : IShape
+    {
+        public double SideA {  get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public double CalculateArea()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(p*(p-SideA)*(p-SideB)*(p-SideC));
+        }
+    }
+}
