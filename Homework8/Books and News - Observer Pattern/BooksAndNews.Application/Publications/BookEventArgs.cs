4a6052bd301646e9e48fae9e0a6cb53ea129fdda
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2023-03-11 12:20:02 +0200

    BooksAndNewsHomework

---
+++ Homework8/Books and News - Observer Pattern/BooksAndNews.Application/Publications/BookEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iQuest.BooksAndNews.Application.Publications
+{
+    public class BookEventArgs : EventArgs
+    {
+        public Book Book;
+
+        public BookEventArgs(Book book)
+        {
+            this.Book = book;
+        }
+    }
+}
---
+++ Homework8/Books and News - Observer Pattern/BooksAndNews.Application/Publications/NewspaperEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iQuest.BooksAndNews.Application.Publications
+{
+    public class NewspaperEventArgs : EventArgs
+    {
+        public Newspaper Newspaper;
+        public NewspaperEventArgs(Newspaper newspaper)
+        {
+            this.Newspaper = newspaper;
+        }
+    }
+}
