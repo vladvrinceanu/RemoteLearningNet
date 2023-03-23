41eec713e52f2631268217557904e136eebcb7d1
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2023-03-24 00:59:05 +0200

    ExerciseOneEqualityTests

---
+++ ObjectEqualityHomework/Terra - Object Equality/Terra - Object Equality/Terra/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace iQuest.Terra
+{
+    public class Country
+    {
+        public string Name { get; }
+
+        public string Capital { get; }
+
+        public Country(string name, string capital)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Capital = capital ?? throw new ArgumentNullException(nameof(capital));
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Country other = (Country)obj;
+
+            return Name == other.Name && Capital == other.Capital;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Capital);
+        }
+    }
+}
