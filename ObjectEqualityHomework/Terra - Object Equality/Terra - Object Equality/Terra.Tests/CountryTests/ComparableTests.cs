2e361a957606ceebcda165ceed2a678910055e41
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2023-03-29 03:13:59 +0300

    ComparableTests

--- ObjectEqualityHomework/Terra - Object Equality/Terra - Object Equality/Terra.Tests/CountryTests/ComparableTests.cs
+++ ObjectEqualityHomework/Terra - Object Equality/Terra - Object Equality/Terra.Tests/CountryTests/ComparableTests.cs
@@ -1,74 +1,74 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace iQuest.Terra.Tests.CountryTests
-//{
-//    [TestClass]
-//    public class ComparableTests
-//    {
-//        [TestMethod]
-//        public void HavingOneCountry_WhenComparedToNull_ReturnsPositive()
-//        {
-//            Country country = new Country("Romania", "Bucharest");
+namespace iQuest.Terra.Tests.CountryTests
+{
+    [TestClass]
+    public class ComparableTests
+    {
+        [TestMethod]
+        public void HavingOneCountry_WhenComparedToNull_ReturnsPositive()
+        {
+            Country country = new Country("Romania", "Bucharest");
 
-//            int actual = country.CompareTo(null);
+            int actual = country.CompareTo(null);
 
-//            Assert.AreEqual(1, actual);
-//        }
+            Assert.AreEqual(1, actual);
+        }
 
-//        [TestMethod]
-//        public void HavingOneCountry_WhenComparedToItself_ReturnsZero()
-//        {
-//            Country country = new Country("Romania", "Bucharest");
+        [TestMethod]
+        public void HavingOneCountry_WhenComparedToItself_ReturnsZero()
+        {
+            Country country = new Country("Romania", "Bucharest");
 
-//            int actual = country.CompareTo(country);
+            int actual = country.CompareTo(country);
 
-//            Assert.AreEqual(0, actual);
-//        }
+            Assert.AreEqual(0, actual);
+        }
 
-//        [TestMethod]
-//        public void HavingOneCountry_WhenComparedToAnotherObject_Throws()
-//        {
-//            Assert.ThrowsException<ArgumentException>(() =>
-//            {
-//                Country country = new Country("Romania", "Bucharest");
-//                object o = new object();
+        [TestMethod]
+        public void HavingOneCountry_WhenComparedToAnotherObject_Throws()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Country country = new Country("Romania", "Bucharest");
+                object o = new object();
 
-//                country.CompareTo(o);
-//            });
-//        }
+                country.CompareTo(o);
+            });
+        }
 
-//        [TestMethod]
-//        public void HavingTwoCountriesWithSameValues_WhenCompared_ReturnsZero()
-//        {
-//            Country country1 = new Country("Romania", "Bucharest");
-//            Country country2 = new Country("Romania", "Bucharest");
+        [TestMethod]
+        public void HavingTwoCountriesWithSameValues_WhenCompared_ReturnsZero()
+        {
+            Country country1 = new Country("Romania", "Bucharest");
+            Country country2 = new Country("Romania", "Bucharest");
 
-//            int actual = country1.CompareTo(country2);
+            int actual = country1.CompareTo(country2);
 
-//            Assert.AreEqual(0, actual);
-//        }
+            Assert.AreEqual(0, actual);
+        }
 
-//        [TestMethod]
-//        public void HavingFirstCountryLessThanSecondCountry_WhenCompared_ReturnsNegative()
-//        {
-//            Country country1 = new Country("France", "Paris");
-//            Country country2 = new Country("Romania", "Bucharest");
+        [TestMethod]
+        public void HavingFirstCountryLessThanSecondCountry_WhenCompared_ReturnsNegative()
+        {
+            Country country1 = new Country("France", "Paris");
+            Country country2 = new Country("Romania", "Bucharest");
 
-//            int actual = country1.CompareTo(country2);
+            int actual = country1.CompareTo(country2);
 
-//            Assert.AreEqual(-1, actual);
-//        }
+            Assert.AreEqual(-1, actual);
+        }
 
-//        [TestMethod]
-//        public void HavingFirstCountryGraterThanSecondCountry_WhenCompared_ReturnsPositive()
-//        {
-//            Country country1 = new Country("Romania", "Bucharest");
-//            Country country2 = new Country("France", "Paris");
+        [TestMethod]
+        public void HavingFirstCountryGraterThanSecondCountry_WhenCompared_ReturnsPositive()
+        {
+            Country country1 = new Country("Romania", "Bucharest");
+            Country country2 = new Country("France", "Paris");
 
-//            int actual = country1.CompareTo(country2);
+            int actual = country1.CompareTo(country2);
 
-//            Assert.AreEqual(1, actual);
-//        }
-//    }
-//}
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
--- ObjectEqualityHomework/Terra - Object Equality/Terra - Object Equality/Terra/Country.cs
+++ ObjectEqualityHomework/Terra - Object Equality/Terra - Object Equality/Terra/Country.cs
@@ -13,6 +13,19 @@ namespace iQuest.Terra
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Capital = capital ?? throw new ArgumentNullException(nameof(capital));
         }
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+            if (obj is Country country)
+            {
+                int name = Name.CompareTo(country.Name);
+                if (name != 0)
+                    return name;
+                return Capital.CompareTo(country.Capital);
+            }
+            throw new ArgumentException("Object is not a Country.");
+        }
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
