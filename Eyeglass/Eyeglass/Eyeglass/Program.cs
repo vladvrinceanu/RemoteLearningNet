0a3b8de35dd95155f3ed72a9aaab397f655d1f85
vladvrinceanu <118467845+vladvrinceanu@users.noreply.github.com>
2023-04-20 23:46:41 +0300

    EyeglassHomework

---
+++ Eyeglass/Eyeglass/Eyeglass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Eyeglass
+{
+    public class Reflection
+    {
+      static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(@"D:\NagarroRemoteLearning\RemoteLearningNet\BuyUseCase\LookUseCase\VendingMachine\bin\Debug\netcoreapp3.1\iQuest.VendingMachine.dll");
+            var MyType = assembly.GetType("iQuest.VendingMachine.Program");
+            dynamic MyObject = Activator.CreateInstance(MyType);
+            Type parameterType = MyObject.GetType();
+           
+
+            Console.WriteLine("All public Fields");
+            foreach(MemberInfo member in parameterType.GetFields()) 
+            {
+                Console.WriteLine(member.Name);
+            }
+
+            Console.WriteLine("All public methods");
+            foreach(MemberInfo member in parameterType.GetMethods())
+            {
+                Console.WriteLine(member.Name);
+            }
+
+            Console.WriteLine("All public properties");
+            foreach(MemberInfo member in parameterType.GetProperties())
+            {
+                Console.WriteLine(member.Name);
+            }
+        }
+    }
+}
